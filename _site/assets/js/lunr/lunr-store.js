7f240a3d49920cc7e73beceec487c0b9e842f57b
balpreet <27923697+khuranabal@users.noreply.github.com>
2022-01-05 15:24:43 +0100

    remove unused posts from template

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Build static website using github pages and jekyll",
-        "excerpt":"Steps to build   Build on local   Deploy to github and configure pages  ","categories": ["blog"],
+        "excerpt":"Steps to build   Build on local   Deploy to github and configure pages    ","categories": ["blog"],
         "tags": ["Jekyll","Github"],
         "url": "/blog/setup-github-pages-with-jekyll/",
         "teaser": null
